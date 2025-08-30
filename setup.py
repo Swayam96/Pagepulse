dc2288c8a9ff8120b991b294eb6c5be273514154
Trisha <swayam.prajapati.962004@gmail.com>
2025-08-30 17:47:54 +0530

    new changes

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 ## edit below variables as per your requirements -
-REPO_NAME = "Books-Recommender-System-Using-Machine-Learning"
-AUTHOR_USER_NAME = "entbappy"
+REPO_NAME = "Pagepulse"
+AUTHOR_USER_NAME = "Swayam96"
 SRC_REPO = "src"
 LIST_OF_REQUIREMENTS = ['streamlit', 'numpy']
 
@@ -13,11 +13,11 @@ LIST_OF_REQUIREMENTS = ['streamlit', 'numpy']
 setup(
     name=SRC_REPO,
     version="0.0.1",
-    author=Swayam Prajapati,
+    author="Swayam Prajapati",
     description="A small package for Movie Recommender System",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    url=f"https://github.com/Swayam96/Pagepulse",
     author_email="swayam.prajapati.200496@gmail.com",
     packages=[SRC_REPO],
     license="MIT",
